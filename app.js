13d159c51acee4d372c115a87ff8b1e3df1545db
CristiTomaaa <cristi.toma1990@gmail.com>
2021-09-20 14:46:41 +0100

    add todo task created and styled

---
+++ app.js
@@ -0,0 +1,45 @@
+// Selectors
+
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event listeners
+
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+
+function addTodo(event) {
+  event.preventDefault();
+  // todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // create <li>
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // complet task button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  // delete task button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+  deleteButton.classList.add("delete-btn");
+  todoDiv.appendChild(deleteButton);
+  // append to list
+  todoList.appendChild(todoDiv);
+  // clear todoInput.value
+  todoInput.value = "";
+}
+
+{
+  /* <div className="todo">
+  <li class="todo-item">hey</li>
+  <button class="complete-btn"><i class="fas fa-check"></i></button>
+  <button class="delete-btn"><i class="fas fa-trash"></button>
+</div> */
+}
